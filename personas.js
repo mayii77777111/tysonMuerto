f0265455f5f579cd71002be74e654544d20991e9
mayii77777111 <mayerlicamachoarancibia7@gmail.com>
2025-11-03 22:05:25 -0300

    Add files via upload

---
+++ personas.js
@@ -0,0 +1,38 @@
+{
+    "personas":"Mi familia",
+    "Lista":
+    [
+        {
+            "Nombre":"magali",
+            "numerof":36,
+            "trabajo":true,
+            "Nacimiento":"1995-05-21"
+        },
+        {
+            "Nombre":"javier",
+            "numerof":37,
+            "trabajo":false,
+            "Nacimiento":"2004-03-31"
+        },
+        {
+            "Nombre":"santino",
+            "numerof":14,
+            "trabajo":true,
+            "Nacimiento":"2000-07-13"
+        },
+
+        {
+            "Nombre":"tyson",
+            "numerof":5,
+            "trabajo":false,
+            "Nacimiento":"2017-04-15"
+        },
+
+        {
+            "Nombre":"india",
+            "numerof":9,
+            "trabajo":false,
+            "Nacimiento":"2019-04-22"
+        },
+    ]
+}
